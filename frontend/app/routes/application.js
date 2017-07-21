f7df6a54ef3a092f8fa19dc89bd824e9f3d6a3a3
Brahian E. Soto Mercedes <brahian@somebrel.com>
2017-07-21 19:15:02 +0200

    Revert "Redirects to the first cate"
    This reverts commit 156fc20639d6bae362e0a8b64bdde6f5ebb316fb.

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -5,9 +5,6 @@ export default Ember.Route.extend({
   beforeModel() {
     return fetch('data.json')
       .then(response => response.json())
-      .then(docData => {
-        this.replaceWith("/" + docData.data.id);
-        return this.store.push(docData);
-      });
+      .then(data => this.store.push(data));
     },
 });
